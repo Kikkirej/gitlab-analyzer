219f2f92e8fde6ccc54c278bafab476a056dd24c
Lemke, Fabian <fabian.lemke@comline.de>
2021-08-12 07:48:56 +0200

    🚧 namespace/basepath checked.

--- gitlab_api/projects.go
+++ gitlab_api/projects.go
@@ -7,34 +7,35 @@ import (
 )
 
 func GetProjects() []*gitlab.Project {
-	git, _ := gitlab.NewClient(settings.Struct.GitlabPersonalToken, gitlab.WithBaseURL(settings.Struct.GitlabBaseurl + "api/v4"))
+	git, _ := gitlab.NewClient(settings.Struct.GitlabPersonalToken, gitlab.WithBaseURL(settings.Struct.GitlabBaseurl+"api/v4"))
 	opt := &gitlab.ListProjectsOptions{
 		ListOptions: gitlab.ListOptions{
-			PerPage:  100,
-			Page: 0,
+			PerPage: 100,
+			Page:    0,
 		},
 	}
 	var projects []*gitlab.Project
 	projects, header, err := git.Projects.ListProjects(opt)
+	log.Println("handled page 1 of", header.TotalPages, "(total pages are not known unless first page is handled)")
 	if err != nil {
-		log.Fatalln("Error while fetching Gitlab-Projects", err)
+		log.Fatalln("error while fetching Gitlab-projects", err)
 	}
-	for page := 1; page <= header.TotalPages; page++ {
-		log.Println("Verarbeite Seite ", page, " von ", header.TotalPages)
+	for page := 2; page <= header.TotalPages; page++ {
+		log.Println("handle page", page, "of", header.TotalPages)
 		optOtherPages := &gitlab.ListProjectsOptions{
 			ListOptions: gitlab.ListOptions{
-				PerPage:  100,
-				Page: page,
+				PerPage: 100,
+				Page:    page,
 			},
 		}
-		addedProjects, _ , err := git.Projects.ListProjects(optOtherPages)
+		addedProjects, _, err := git.Projects.ListProjects(optOtherPages)
 		if err != nil {
-			log.Fatalln("Error while fetching Gitlab-Projects", err)
+			log.Fatalln("error while fetching Gitlab-projects", err)
 		}
 		projects = append(projects, addedProjects...)
 	}
 	if err != nil {
-		log.Fatalln("Error while fetching Gitlab-Projects", err)
+		log.Fatalln("error while fetching Gitlab-projects", err)
 	}
 	return projects
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/xanzy/go-gitlab"
 	"gitlabAnalyzer/settings"
 	"log"
+	"strings"
 )
 import "gitlabAnalyzer/gitlab_api"
 
@@ -11,5 +13,23 @@ import "gitlabAnalyzer/gitlab_api"
 func main() {
 	settings.InitSettings()
 	projects := gitlab_api.GetProjects()
-	log.Println("number of identified projects: ", len(projects) )
+	log.Println("number of identified projects:", len(projects))
+	for _, project := range projects {
+		if shouldBeAnalyzed(project) {
+			log.Println("analyze Project:", project.PathWithNamespace, " (", project.ID, ")")
+
+		} else {
+			log.Println("project does not meet criteria:", project.PathWithNamespace, "(", project.ID, ")")
+		}
+	}
+}
+
+func shouldBeAnalyzed(project *gitlab.Project) bool {
+	if settings.Struct.GitlabProjectRoot == "" {
+		return true
+	}
+	if strings.HasPrefix(project.PathWithNamespace, settings.Struct.GitlabProjectRoot) {
+		return true
+	}
+	return false
 }
